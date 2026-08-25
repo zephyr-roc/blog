822b40c5e021afa008ed0d581c4b2180fd41292f
Zephyr <94433344+zephyr-roc@users.noreply.github.com>
2026-08-25 11:16:06 +0800

    perf: add material-only liquid glass core

---
+++ app/components/liquid-glass/index.ts
@@ -0,0 +1,3 @@
+export { GlassMaterial as Glass } from "./GlassMaterial";
+export type { GlassMaterialProps as GlassProps } from "./GlassMaterial";
+export type { GlassOptics } from "./displacement";
