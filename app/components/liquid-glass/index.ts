ccc0076b0793cf370d809da1e1f531a9aa356f89
Zephyr <94433344+zephyr-roc@users.noreply.github.com>
2026-08-26 19:44:04 +0800

    refactor: export liquid glass support hook

--- app/components/liquid-glass/index.ts
+++ app/components/liquid-glass/index.ts
@@ -1,3 +1,3 @@
-export { GlassMaterial as Glass } from "./GlassMaterial";
+export { GlassMaterial as Glass, useLiquidGlassSupport } from "./GlassMaterial";
 export type { GlassMaterialProps as GlassProps } from "./GlassMaterial";
 export type { GlassOptics } from "./displacement";
